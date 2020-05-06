81299106461a0f4605b32af426b9f1600446a4b7
martinkonak <martin.konak@tul.cz>
2020-05-06 10:36:57 +0200

    cmd

--- 09_Cmd/src/cmd/Cd.java
+++ 09_Cmd/src/cmd/Cd.java
@@ -21,18 +21,9 @@ public class Cd extends Command{
             CmdEditor.changeActualDir(directory);
             return "Directory changed";
         }else if(params[1].equals("..")){
-            String oldDir = actualDir.getAbsolutePath();
-            System.out.println("1");
-            System.out.println(oldDir);
-            String[] p = oldDir.split("\\");
-            System.out.println("2");
-            String finalDir = p[p.length-2];
-            System.out.println("3");
-            File fDir = new File(finalDir).getAbsoluteFile();
-            System.out.println("4");
-            CmdEditor.changeActualDir(fDir);
-            System.out.println("5");
-            return "Directory changed2";
+            File nDir = actualDir.getParentFile();
+            CmdEditor.changeActualDir(nDir);
+            return "Directory changed";
         }else{
             return "Selected folder does not exist\n";
             //return directory.getAbsolutePath();
--- 09_Cmd/src/cmd/Parser.java
+++ 09_Cmd/src/cmd/Parser.java
@@ -13,24 +13,27 @@ public class Parser{
     
     public static Command parse(String line){
         String[] p = line.split(" +"); //p[0] dir; p[1] -e; p[2] .java
-        
         //TODO prázdný příkaz
-        /*if (p.length == 0) {
+        if (p == null) {
             return null;
-        }*/
-        try{
-            char first = Character.toUpperCase(p[0].charAt(0));
-            String name = Command.COMMAND_PACKAGE + "." + first + p[0].substring(1);
-            try{
-                Class c = Class.forName(name);
-                Command command = (Command)c.newInstance();
-                command.setParams(p);
-                return command;
-            } catch(Exception e){
-                throw new RuntimeException("Parse failed");
-            }  
-        }catch(Exception f){
-            throw new RuntimeException("P");
         }
+        char first = 0;
+        
+        try{
+            first = Character.toUpperCase(p[0].charAt(0));
+        }catch(Exception f){
+            throw new RuntimeException("");
+        }
+        
+        String name = Command.COMMAND_PACKAGE + "." + first + p[0].substring(1);
+        try{
+        Class c = Class.forName(name);
+            Command command = (Command)c.newInstance();
+            command.setParams(p);
+            return command;
+        } catch(Exception e){
+            throw new RuntimeException("Parse failed\n");
+        }  
+
     }
 }
